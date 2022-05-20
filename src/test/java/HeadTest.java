3a6a77441d7f43530a42dd2289fb29b1e569f0e5
KaterinaTerekhova/GitBas <Terekhovaca@gmail.com>
2022-05-20 11:55:14 -0400

    add Test

---
+++ src/test/java/HeadTest.java
@@ -0,0 +1,32 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class HeadTest {
+
+    @Test
+    public void testHeadTitle() throws InterruptedException {
+        String chromeDriver = "webdriver.chrome.driver";
+        String driverPath = "/C:/Users/Королевна/chromedriver_win32/chromedriver.exe";
+        String url = "https://www.99-bottles-of-beer.net/";
+
+        String expectedResult = "99 Bottles of Beer";
+
+        System.setProperty(chromeDriver,driverPath);
+        WebDriver driver = new ChromeDriver();
+        driver.get(url);
+        WebElement menuHead = driver.
+                findElement(By.xpath("//body/div[@id='wrap']/div[@id='header']/h1")
+                );
+        menuHead.click();
+
+        String actualResult = menuHead.getText();
+        Assert.assertEquals(actualResult,expectedResult);
+
+        Thread.sleep(2000);
+        driver.quit();
+    }
+}

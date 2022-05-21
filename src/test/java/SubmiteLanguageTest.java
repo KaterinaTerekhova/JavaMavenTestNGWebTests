e49d9467ae4bfb3ca223f740e209413a6fc27011
KaterinaTerekhova/GitBas <Terekhovaca@gmail.com>
2022-05-21 19:49:53 -0400

    add test#11

---
+++ src/test/java/SubmiteLanguageTest.java
@@ -0,0 +1,47 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SubmiteLanguageTest {
+
+    @Test
+    public void testSubmiteLanguage(){
+
+        String chromeDriver = "webdriver.chrome.driver";
+        String driverPath = "/C:/Users/Королевна/chromedriver_win32/chromedriver.exe";
+        String url = "http://www.99-bottles-of-beer.net/submitnewlanguage.html";
+
+        String expectedResult = "Error: Precondition failed - Incomplete Input.";
+
+        System.setProperty(chromeDriver, driverPath);
+        WebDriver driver = new ChromeDriver();
+        driver.get(url);
+
+        WebElement menuSubmiteButton = driver.
+                findElement(By.xpath("//body/div[@id='wrap']/div[@id='main']/form[@id='addlanguage']/p/input[@type='submit']")
+                );
+        menuSubmiteButton.click();
+
+        WebElement menuError = driver.
+                findElement(By.xpath("//body/div[@id='wrap']/div[@id='main']/form[@id='addlanguage']/p/input[@type='submit']")
+                );
+
+        WebElement menuTextOfError = driver.
+                findElement(By.xpath("//body/div[@id='wrap']/div[@id='main']/p"));
+
+        String actualResult = menuTextOfError.getText();
+        Assert.assertEquals(actualResult,expectedResult);
+        driver.quit();
+
+
+
+
+
+
+
+
+    }
+}

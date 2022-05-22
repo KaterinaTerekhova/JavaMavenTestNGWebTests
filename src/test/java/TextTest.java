ba9df8e225d4782b357a4cd405e46b28f26cd833
KaterinaTerekhova/GitBas <Terekhovaca@gmail.com>
2022-05-21 22:24:21 -0400

    add test#13

---
+++ src/test/java/TextTest.java
@@ -0,0 +1,45 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TextTest {
+
+    @Test
+    public void testSubmiteLanguage() {
+
+        String chromeDriver = "webdriver.chrome.driver";
+        String driverPath = "/C:/Users/Королевна/chromedriver_win32/chromedriver.exe";
+        String url = "http://www.99-bottles-of-beer.net/submitnewlanguage.html";
+
+        String expectedResult = "IMPORTANT: Take your time! The more carefully you fill out this form " +
+                "(especially the language name and description), the easier it will be for us and the faster" +
+                " your language will show up on this page. We don't have the time to mess around with fixing " +
+                "your descriptions etc. Thanks for your understanding.";
+
+        System.setProperty(chromeDriver, driverPath);
+        WebDriver driver = new ChromeDriver();
+        driver.get(url);
+
+        WebElement menuSubmiteButton = driver.
+                findElement(By.xpath("//body/div[@id='wrap']/div[@id='main']/form[@id='addlanguage']/p/input[@type='submit']")
+                );
+        menuSubmiteButton.click();
+
+        WebElement menuError = driver.
+                findElement(By.xpath("//body/div[@id='wrap']/div[@id='main']/form[@id='addlanguage']/p/input[@type='submit']")
+                );
+
+        WebElement menuTextOfError = driver.
+                findElement(By.xpath("//body/div[@id='wrap']/div[@id='main']/p"));
+
+        WebElement important = driver.
+                findElement(By.xpath("//body/div[@id='wrap']/div[@id='main']/ul/li"));
+
+        String actualResult = important.getText();
+        Assert.assertEquals(actualResult, expectedResult);
+        driver.quit();
+    }
+}

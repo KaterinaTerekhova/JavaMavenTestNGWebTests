3c33bc4fa247072c333c169e5701158437c495d4
KaterinaTerekhova/GitBas <Terekhovaca@gmail.com>
2022-05-21 23:55:53 -0400

    add test#14

---
+++ src/test/java/BrowseLanguageTablTest.java
@@ -0,0 +1,47 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class BrowseLanguageTablTest {
+
+    @Test
+    public void testBrowseLanguageTabl() {
+
+        String chromeDriver = "webdriver.chrome.driver";
+        String driverPath = "/C:/Users/Королевна/chromedriver_win32/chromedriver.exe";
+        String url = "http://www.99-bottles-of-beer.net";
+
+        String expectedResultFirstColumn = "Language";
+        String expectedResultSecondColumn = "Author";
+
+        System.setProperty(chromeDriver, driverPath);
+        WebDriver driver = new ChromeDriver();
+        driver.get(url);
+
+        WebElement firstSubtitle = driver.
+                findElement(By.xpath("//body/div[@id='wrap']/div[@id='navigation']" +
+                        "/ul[@id='menu']/li/a[@href='/abc.html']")
+                );
+        firstSubtitle.click();
+
+        WebElement firstColumn = driver.
+                findElement(By.xpath("//body/div[@id='wrap']/div[@id='main']/table[@id='category']" +
+                        "/tbody/tr/th[1]")
+        );
+
+        String actualResultFirstColumn = firstColumn.getText();
+        Assert.assertEquals(actualResultFirstColumn,expectedResultFirstColumn);
+
+        WebElement secodColumn = driver.
+                findElement(By.xpath("//body/div[@id='wrap']/div[@id='main']/table[@id='category']" +
+                        "/tbody/tr/th[2]")
+                );
+        String actualResultSecondColumn = secodColumn.getText();
+        Assert.assertEquals(actualResultSecondColumn,expectedResultSecondColumn);
+
+        driver.quit();
+    }
+}
